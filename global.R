f8d394bced219b8caa7df04fa118e3e0760a15cf
matiaswak <32601694+matiaswak@users.noreply.github.com>
2019-07-30 12:33:19 -0300

    Update and rename global.R to app.R

--- global.R
+++
@@ -1,8 +0,0 @@
-options(repos = 'https://cran.rstudio.com/')
-
-if(suppressMessages(suppressWarnings(!require(digest)))){install.packages("digest");library(digest)}
-if(suppressMessages(suppressWarnings(!require(flexdashboard)))){install.packages("flexdashboard");library(flexdashboard)}
-if(suppressMessages(suppressWarnings(!require(shinydashboard)))){install.packages("shinydashboard");library(shinydashboard)}
-if(suppressMessages(suppressWarnings(!require(shinyjs)))){install.packages("shinyjs");library(shinyjs)}
-if(suppressMessages(suppressWarnings(!require(tidyverse)))){install.packages("tidyverse");library(tidyverse)}
-if(suppressMessages(suppressWarnings(!require(shinyalert)))){install.packages("shinyalert");library(shinyalert)}
